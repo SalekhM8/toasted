ab66c80f3c8ccd851de39a1c39ac165049648b9f
SalekhM8 <SALEKH.MAHMOod1@gmail.com>
2025-01-26 16:09:27 +0000

    state change

--- backend/src/utils/generateToken.js
+++ backend/src/utils/generateToken.js
@@ -2,13 +2,13 @@ const jwt = require('jsonwebtoken');
 
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: '30m'  // Short lived access token
+    expiresIn: '1000d'  // Changed from '30m' to '365d'
   });
 };
 
 const generateRefreshToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_REFRESH_SECRET, {
-    expiresIn: '30d'  // Longer lived refresh token
+    expiresIn: '1000d'  // Changed from '30d' to '365d' to match
   });
 };
 
